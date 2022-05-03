7a66c12afc6f6a5c1bdccdf1522a07f276d888c0
juldblack <juliendunia@hotmail.com>
2022-05-03 09:21:53 +0200

    Création Base de données, PinController, modif .env

---
+++ src/Controller/PinController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PinController extends AbstractController
+{
+    //#[Route('/', name: 'app_home')]
+    
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function index(): Response
+    {
+        /*return $this->render('pin/index.html.twig', [
+            'controller_name' => 'PinController',
+        ]);*/
+        return new Response("Hello Julien Dunia");
+        //return $this->json(['message' => 'Hello World']);
+    }
+}
